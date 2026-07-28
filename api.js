ee784965fc47c9dfeb17f02ba267fda9e768ae24
Samilo Ferreira Viana <samiloviana@outlook.com>
2026-07-28 08:59:58 -0300

    nova api

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ import fs from "node:fs/promises"
 
 async function cambio() {
     
-const pesquisaapi = await fetch("https://api.exchangerate.fun/latest?base=USD")
+const pesquisaapi = await fetch("https://v6.exchangerate-api.com/v6/1015227165fabdcf141f7bf2/latest/USD")
 const dados = await pesquisaapi.json()
 
 await fs.writeFile("cotacao.json", JSON.stringify(dados))
